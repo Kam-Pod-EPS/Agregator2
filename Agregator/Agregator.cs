89042fb99b5302f5674b05800d7f7844ec310714
MakhovychOleksandr <umismag@gmail.com>
2017-11-24 21:20:32 +0200

    Add project files.

---
+++ Agregator/Agregator.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using RequestData;
+
+namespace AgregatorNS
+{
+	public class NewAgregator : Agregator
+	{
+		string url;
+		public string Url
+		{
+			get => url;
+			set => url = value;
+		}
+
+		string account;
+		public string Account
+		{
+			get => account;
+			set => account = value;
+		}
+
+		string secretKey;
+		public string SecretKey
+		{
+			get => secretKey;
+			set => secretKey = value;
+		}
+
+		string terminalId = null;
+		public string TerminalId
+		{
+			get => terminalId;
+			set => terminalId = value;
+		}
+
+		string certificateName;
+		public string CertificateName
+		{
+			get => certificateName;
+			set => certificateName = value;
+		}
+
+		string certificatePassword;
+		public string CertificatePassword
+		{
+			get => certificatePassword;
+			set => certificatePassword = value;
+		}
+
+		HttpClient httpClient = null;
+
+		public NewAgregator()
+		{
+			Initialize("http://localhost", "User1", "VeryBigSecret", "001", "certificate1", "321");
+		}
+
+		public override CheckResponse Check(int providerProductId, string jsonFields)
+		{
+			CheckResponse checkResponse = new CheckResponse();
+
+			Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+
+			dictionary.Add("IdService", providerProductId.ToString());
+			//Parse json fields
+
+			Encoding win1251 = Encoding.GetEncoding(1251);
+			HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, Url);
+			httpRequestMessage.Content = new StringContent(jsonFields, win1251);
+			httpRequestMessage.Version = HttpVersion.Version10;
+
+			try
+			{
+				X509Certificate2 store = null;
+				try
+				{
+					store = new X509Certificate2("certificate.pem");
+				}
+				catch(Exception e)
+				{
+					//
+				}
+
+				var clientHandler = new WebRequestHandler();
+				clientHandler.ClientCertificates.Add(store);
+				clientHandler.UseDefaultCredentials = true;
+
+				httpClient = new HttpClient(clientHandler);
+				var uri = new Uri(Url);
+				httpClient.BaseAddress = uri;
+
+				System.Net.Security.SslStream sslStream = new System.Net.Security.SslStream(httpClient.GetStreamAsync(Url).Result);
+				sslStream.AuthenticateAsClient(Url);
+
+				using (HttpResponseMessage responseMessage = httpClient.SendAsync(httpRequestMessage).Result)
+				{
+					if (responseMessage.IsSuccessStatusCode)
+					{
+						var responseTask = responseMessage.Content.ReadAsStringAsync();
+						responseTask.Wait();
+						checkResponse.JsonValue = responseTask.Result;
+						checkResponse.Status = true;
+					}
+					else
+					{
+						checkResponse.Status = false;
+						checkResponse.ErrorCode = responseMessage.StatusCode.ToString();
+						checkResponse.ErrorMessage = responseMessage.ReasonPhrase;
+
+						throw new Exception(responseMessage.StatusCode + ": " + responseMessage.ReasonPhrase);
+					}
+				}
+			}
+			catch (WebException e)
+			{
+				checkResponse.Status = false;
+				checkResponse.ErrorCode = null;
+				checkResponse.ErrorMessage = e.Message;
+			}
+			catch (InvalidOperationException e)
+			{
+				checkResponse.Status = false;
+				checkResponse.ErrorCode = null;
+				checkResponse.ErrorMessage = e.Message;
+			}
+			catch (HttpRequestException e)
+			{
+				checkResponse.Status = false;
+				checkResponse.ErrorCode = null;
+				checkResponse.ErrorMessage = e.Message;
+			}
+			catch (Exception e)
+			{
+				checkResponse.Status = false;
+				checkResponse.ErrorCode = null;
+				checkResponse.ErrorMessage = e.Message;
+			}
+
+			//content.Dispose();
+			return checkResponse;
+		}
+
+		public CheckResponse Check_old(int providerProductId, string jsonFields)
+		{
+			CheckResponse checkResponse = new CheckResponse();
+
+			WebRequest webRequest = WebRequest.Create(Url);
+			webRequest.Method = "POST";
+			Encoding encoding = Encoding.GetEncoding(1251);
+			byte[] dataArray = encoding.GetBytes(jsonFields);
+			webRequest.ContentType = "text/XML";
+			webRequest.ContentLength = dataArray.Length;
+
+
+
+			try
+			{
+				using (Stream webRequestStream = webRequest.GetRequestStream())
+				{
+					webRequestStream.Write(dataArray, 0, dataArray.Length);
+				}
+
+				WebResponse webResponse = webRequest.GetResponse();
+
+				using (Stream webResponseStream = webResponse.GetResponseStream())
+				{
+					using (StreamReader sr = new StreamReader(webResponseStream))
+					{
+						checkResponse.JsonValue = sr.ReadToEnd();
+					}
+				}
+			}
+			catch (WebException e)
+			{
+				checkResponse.Status = false;
+				checkResponse.ErrorCode = e.Status.ToString();
+				checkResponse.ErrorMessage = e.Message;
+			}
+
+
+
+			return checkResponse;
+		}
+
+		public override CommissionResponse Commission(int providerProductId, string jsonFields)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string GetFields(int providerProductId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override object GetServices()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Initialize(string url, string account, string secretKey, string terminalId, string certificateName, string certificatePassword)
+		{
+			Url = url;
+			Account = account;
+			SecretKey = secretKey;
+			TerminalId = terminalId;
+			CertificateName = certificateName;
+			CertificatePassword = certificatePassword;
+		}
+
+		public override PayResponse Pay(int providerProductId, string jsonFields, string idempotentKey)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ RequestData/RequestStructure.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace RequestData
+{
+	public class RtHeader
+	{
+		public int idMsgType { get; set; }
+	}
+
+	public class Body
+	{
+		Dictionary<string, string> Items { get; set; } = new Dictionary<string, string>();
+	}
+
+	public class RFooter
+	{
+		string msgDateTime;
+		public string MsgDateTime
+		{
+			get {return msgDateTime;}
+			set {msgDateTime = value;}
+		}
+
+		string idKeyField;
+		public string idKey
+		{
+			get { return idKeyField; }
+			set { idKeyField = value; }
+		}
+
+		public RFooter() { }
+
+		public RFooter(string msgDateTime, string idKey)
+		{
+			MsgDateTime = msgDateTime;
+			this.idKey = idKey;
+		}
+	}
+
+	public class Request
+	{
+		public RtHeader RtHeader { get; set; }
+		public Body body { get; set; }
+		public RFooter RFooter { get; set; }
+
+		public Request() { }
+
+		public Request(RtHeader rtHeader, Body body, RFooter rtFooter)
+		{
+			RtHeader = rtHeader;
+			this.body = body;
+			RFooter = rtFooter;
+		}
+	}
+	
+
+
+	public class Data
+    {
+		public Request Request { get; set; }
+		public string SignMsg { get; set; }
+
+		public Data() { }
+
+		public Data(Request request, string signMsg)
+		{
+			Request = request;
+			SignMsg = signMsg;
+		}
+    }
+
+	public static class Serialization
+	{
+		public static string Obj2XMLstring(object obj)
+		{
+			string res;
+			Encoding win1251 = Encoding.GetEncoding(1251);
+			XmlSerializer xmlserializer = new XmlSerializer(obj.GetType());
+			byte[] bytes;
+			char[] chars;
+
+			using (Stream stream=new MemoryStream())
+			{
+				xmlserializer.Serialize(stream, obj);
+				stream.Seek(0, SeekOrigin.Begin);
+
+				bytes = new byte[stream.Length];
+				stream.ReadAsync(bytes, 0, (int)stream.Length);
+
+				chars = new char[win1251.GetCharCount(bytes)];
+				win1251.GetDecoder().GetChars(bytes, 0, bytes.Length, chars, 0, true);
+				res =new string(chars);
+			}
+			return res;
+		}
+
+	}
+}
